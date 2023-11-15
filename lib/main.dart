b720b8e2b93edb9a3a7fba07c9122ccd3746bdae
JasonSunaryo <jasonsunaryo01@gmail.com>
2023-11-15 15:17:33 +0700

    hasil

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,120 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        appBar: MyAppBar(), // Use your custom AppBar
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: MyAppBar(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: FractionalTranslation(
+                        translation: Offset(0.0, -0.0),
+                        child: Image(
+                          image: AssetImage('img/clothes-hanger.png'),
+                          width: 100,
+                          height: 100,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Rp 10,000',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    BlueContainer1(),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: FractionalTranslation(
+                        translation: Offset(0.0, -0.0),
+                        child: Image(
+                          image: AssetImage('img/jacket.png'),
+                          width: 100,
+                          height: 100,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Rp 20,000',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    BlueContainer1(),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 8.0), // Adjusted spacing
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: FractionalTranslation(
+                        translation: Offset(0.0, -0.0),
+                        child: Image(
+                          image: AssetImage('img/soap.png'),
+                          width: 100,
+                          height: 100,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Rp 30,000',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    BlueContainer1(),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: FractionalTranslation(
+                        translation: Offset(0.0, -0.0),
+                        child: Image(
+                          image: AssetImage('img/steak.png'),
+                          width: 100,
+                          height: 100,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Rp 40,000',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    BlueContainer1(),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -29,12 +134,19 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      backgroundColor: Colors.orange, // Set the background color to orange
+      backgroundColor: Colors.orange,
       leading: IconButton(
         icon: Icon(Icons.arrow_back),
         onPressed: () {
           // Handle the back button press
         },
+      ),
+      title: Text(
+        'Godrej',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
       ),
       actions: [
         IconButton(
@@ -44,6 +156,27 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
           },
         ),
       ],
+    );
+  }
+}
+
+class BlueContainer1 extends StatelessWidget {
+  const BlueContainer1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 8.0), // Add margin to separate image and container
+      color: Colors.blue,
+      padding: const EdgeInsets.all(16.0),
+      child: Text(
+        '+Keranjang',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
